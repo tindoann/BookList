29356bdf4ebf078e64d402bc34992d9e1a9059bb
tindoann <35037427+tindoann@users.noreply.github.com>
2019-01-21 15:35:55 -0500

    Update Book.js

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,6 +1,39 @@
 import React, { Component } from 'react'
 
 class Book extends Component {
+
+  constructor(props){
+    super(props)
+    this.state={
+      count:0,
+    };
+  }
+    // this.handleClick = this.handleClick.bind(this)
+  
+
+
+
+// handleClick(){
+//   console.log('you clicked me'); 
+//   console.log(this.state.count);
+// }
+
+addCount = () => {
+  this.setState({
+    count: this.state.count + 1
+  }); 
+}; 
+lowerCount = () => {
+  this.setState({
+    count: this.state.count - 1
+  }); 
+}; 
+resetCount = () => {
+  this.setState({
+    count: 0
+  }); 
+}; 
+
   render() {
     const { img, title, author } = this.props.info; 
     return (
@@ -9,6 +42,10 @@ class Book extends Component {
         <div>
           <h4>Title : {title}</h4>
           <h6>BY : {author}</h6>
+          <button onClick={this.addCount}>Add Count</button>
+          <button onClick={this.lowerCount}>Lower Count</button>
+          <button onClick={this.resetCount}>Reset Button</button>
+          <h1>count : {this.state.count} </h1>
         </div>
       </div>
     )
@@ -16,5 +53,3 @@ class Book extends Component {
 }
 
 export default Book; 
-
-
